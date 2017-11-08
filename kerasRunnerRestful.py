fdc970077b51a5712729262af694ee2c2cd51e1e
root <root@ip-172-31-18-188.eu-west-1.compute.internal>
2017-11-08 01:38:51 +0000

    Data row input from req works

--- kerasRunnerRestful.py
+++ kerasRunnerRestful.py
@@ -4,22 +4,9 @@ from flask_restful import Resource, Api
 import numpy as np
 from keras.models import model_from_json
 import timeit
-
-#json_file = open('models/bidirectionalClassLstmLongswordModel.json', 'r')
-#loaded_model_json = json_file.read()
-#json_file.close()
-#loaded_model = model_from_json(loaded_model_json)
-#loaded_model.load_weights("models/bidirectionalClassLstmLongswordModelWeights.h5")
-
-#x_test = np.array([[ 0, 8370, 0,10747, 0, 9541, 0, 240, 0, 456, 0, 27]])
-#prediction = loaded_model.predict(x_test)
-#predictionArgMax = np.argmax(prediction, axis=1)
-#print('Class: ', predictionArgMax[0])
-#print('Confidence: ', prediction[0, predictionArgMax[0]])
-
+import json
 
 # Flask
-
 app = Flask(__name__)
 api = Api(app)
 
@@ -27,15 +14,26 @@ class Predict(Resource):
     def put(self):
         dataRow = request.form['data']
 
+	# Load model
 	json_file = open('models/bidirectionalClassLstmLongswordModel.json', 'r')
 	loaded_model_json = json_file.read()
 	json_file.close()
 	loaded_model = model_from_json(loaded_model_json)
 	loaded_model.load_weights("models/bidirectionalClassLstmLongswordModelWeights.h5")
 
-
+	# Input data
 	x_test = np.array([[ 0, 8370, 0,10747, 0, 9541, 0, 240, 0, 456, 0, 27]])
+	#x_test2 = json.loads(dataRow)
 
+	#print ('xtest_2', x_test2)
+
+	#x_test3 = x_test2["row"]
+
+	#x_test = np.array([x_test3])
+	x_test = np.array([json.loads(dataRow)["row"]])
+
+
+	# Predict
 	start_time = timeit.default_timer()
 	prediction = loaded_model.predict(x_test)
 	elapsed = timeit.default_timer() - start_time
@@ -46,4 +44,5 @@ class Predict(Resource):
 api.add_resource(Predict, '/predict')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+#    app.run(debug=True)
+     app.run()
