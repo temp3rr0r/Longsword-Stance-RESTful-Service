f7c510551ab6badb564c8dbaf0ecc9fd7c94ad9e
root <root@ip-172-31-18-188.eu-west-1.compute.internal>
2017-11-08 01:49:53 +0000

    Before opening port

--- kerasRunnerRestful.py
+++ kerasRunnerRestful.py
@@ -22,27 +22,16 @@ class Predict(Resource):
 	loaded_model.load_weights("models/bidirectionalClassLstmLongswordModelWeights.h5")
 
 	# Input data
-	x_test = np.array([[ 0, 8370, 0,10747, 0, 9541, 0, 240, 0, 456, 0, 27]])
-	#x_test2 = json.loads(dataRow)
-
-	#print ('xtest_2', x_test2)
-
-	#x_test3 = x_test2["row"]
-
-	#x_test = np.array([x_test3])
 	x_test = np.array([json.loads(dataRow)["row"]])
-
 
 	# Predict
 	start_time = timeit.default_timer()
 	prediction = loaded_model.predict(x_test)
 	elapsed = timeit.default_timer() - start_time
-	predictionArgMax = np.argmax(prediction, axis=1)	
-
-        return { 'predictedClass': predictionArgMax[0], 'confidence': str(prediction[0, predictionArgMax[0]]), 'elapsedMilliseconds': elapsed * 1000 }
+	predictionArgMax = np.argmax(prediction, axis=1)
+        return { 'predictedClass': predictionArgMax[0], 'confidence': float(prediction[0, predictionArgMax[0]]), 'elapsedMilliseconds': elapsed * 1000 }
 
 api.add_resource(Predict, '/predict')
 
 if __name__ == '__main__':
-#    app.run(debug=True)
      app.run()
