b06219f25f48f9398939b74a8f78164433ed2db5
root <root@ip-172-31-18-188.eu-west-1.compute.internal>
2017-11-08 15:01:54 +0000

    First chain that fully works

--- kerasRunnerRestful.py
+++ kerasRunnerRestful.py
@@ -24,6 +24,21 @@ class Predict(Resource):
 
 	# Input data
 	x_test = np.array([json_data['row']])
+	
+	r = x_test
+	r2 = np.copy(r)
+	r[r < 0] = 0
+	r2[r2 > 0] = 0
+	r2 *= -1
+	r = np.insert(r, 0, values=r2[:,0], axis=1)        
+	r = np.insert(r, 1, values=r2[:,1], axis=1)
+	r = np.insert(r, 2, values=r2[:,2], axis=1)
+	r = np.insert(r, 3, values=r2[:,3], axis=1)
+	r = np.insert(r, 4, values=r2[:,4], axis=1)
+	r = np.insert(r, 5, values=r2[:,5], axis=1)
+	x_test = r
+
+	#print("x_test", x_test)
 
 	# Predict
 	start_time = timeit.default_timer()
